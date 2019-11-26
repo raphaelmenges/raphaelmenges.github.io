1e03e4c641a73fd11be9e84d0f8ac56a17c43fac
Raphael Menges <unrealer2@yahoo.de>
2019-11-26 14:25:08 +0100

    Prohibit scrolling behind lightbox.

--- js/lightbox.js
+++ js/lightbox.js
@@ -1,7 +1,6 @@
 function showLightbox(image, description) {
 
 	var body = document.getElementsByTagName('BODY')[0];
-	// body.setAttribute('class', 'noscroll'); // prohibit scrolling of body
 
 	// Table
 	var a = document.createElement('a');
@@ -32,6 +31,9 @@ function showLightbox(image, description) {
 
 	// Push state for lightbox
 	window.history.pushState('', '', '?lightbox')
+
+	// Prevent scrolling
+	disableScroll();
 }
 
 window.onpopstate = function(event) {
@@ -45,5 +47,48 @@ function hideLightbox() {
 		body.removeChild(lightbox);
 		window.history.replaceState('', '', '/');
 	}
-	// body.classList.remove('noscroll'); // allow scrolling of body, again
+
+	// Allow scrolling
+	enableScroll();
+}
+
+// Prohibit scrolling of the body while lightbox is visible
+// https://stackoverflow.com/questions/4770025/how-to-disable-scrolling-temporarily
+
+// left: 37, up: 38, right: 39, down: 40,
+// spacebar: 32, pageup: 33, pagedown: 34, end: 35, home: 36
+var keys = {37: 1, 38: 1, 39: 1, 40: 1};
+
+function preventDefault(e) {
+	e = e || window.event;
+	if (e.preventDefault)
+			e.preventDefault();
+	e.returnValue = false;
+}
+
+function preventDefaultForScrollKeys(e) {
+	if (keys[e.keyCode]) {
+		preventDefault(e);
+		return false;
+	}
+}
+
+function disableScroll() {
+	if (window.addEventListener) // older FF
+		window.addEventListener('DOMMouseScroll', preventDefault, false);
+	document.addEventListener('wheel', preventDefault, {passive: false}); // Disable scrolling in Chrome
+	window.onwheel = preventDefault; // modern standard
+	window.onmousewheel = document.onmousewheel = preventDefault; // older browsers, IE
+	window.ontouchmove = preventDefault; // mobile
+	document.onkeydown = preventDefaultForScrollKeys;
+}
+
+function enableScroll() {
+	if (window.removeEventListener)
+		window.removeEventListener('DOMMouseScroll', preventDefault, false);
+	document.removeEventListener('wheel', preventDefault, {passive: false}); // Enable scrolling in Chrome
+	window.onmousewheel = document.onmousewheel = null; 
+	window.onwheel = null; 
+	window.ontouchmove = null;
+	document.onkeydown = null;
 }
