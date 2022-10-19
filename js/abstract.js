468676cb9b36cf8c21c9b09e100578423aea9bbf
raphaelmenges <unrealer2@yahoo.de>
2022-10-19 15:01:31 +0000

    Deploying to master from deploy b6de49b60a99dd234182e4a6c0cdfbba661071dc

---
+++ js/abstract.js
@@ -0,0 +1,44 @@
+/* Does not work in IE */
+var less_count = 200;
+var abstract_list = [];
+var abstract_nodes = document.getElementsByClassName('abstract');
+for (var i = 0; i < abstract_nodes.length; i++) {
+  var text = abstract_nodes[i].innerText;
+  if (text.length > less_count) {
+    /* Replace innerText with two spans*/
+    abstract_nodes[i].innerText = ''
+
+    var less_text = document.createElement('span');
+    less_text.innerText = text.slice(0, less_count) + '...';
+    less_text.setAttribute('style', 'display: inline');
+    abstract_nodes[i].appendChild(less_text);
+
+    var full_text = document.createElement('span');
+    full_text.innerText = text;
+    full_text.setAttribute('style', 'display: none');
+    abstract_nodes[i].appendChild(full_text);
+
+    var a = document.createElement('a');
+    a.setAttribute('href', 'javascript:toogleAbstract(' + abstract_list.length + ')');
+    a.innerText = '[show more]';
+    a.setAttribute('style', 'margin-left: 2px');
+    abstract_nodes[i].appendChild(a);
+
+    abstract_list.push([less_text, full_text, a, false]); // bool indicated less (false) or full (true)
+  }
+}
+
+function toogleAbstract(idx) {
+  item = abstract_list[idx];
+  if(item[3]) { // full text to less text
+    item[0].setAttribute('style', 'display: inline');
+    item[1].setAttribute('style', 'display: none');
+    item[2].innerText = '[show more]';
+    item[3] = false;
+  } else { // less text to full text
+    item[0].setAttribute('style', 'display: none');
+    item[1].setAttribute('style', 'display: inline');
+    item[2].innerText = '[show less]';
+    item[3] = true;
+  }
+}
