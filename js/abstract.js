bd0e76e3b7254c37d02108d5aa353cff0919bf17
raphaelmenges <unrealer2@yahoo.de>
2025-02-04 19:07:40 +0000

    Deploying to gh_pages from @ raphaelmenges/raphaelmenges.github.io@8e2821305868d6274c8cb3416475a7692a5d2cc7 🚀

--- js/abstract.js
+++ js/abstract.js
@@ -1,36 +1,36 @@
 /* Does not work in IE */
-var less_count = 200;
-var abstract_list = [];
-var abstract_nodes = document.getElementsByClassName('abstract');
-for (var i = 0; i < abstract_nodes.length; i++) {
-  var text = abstract_nodes[i].innerText;
+let less_count = 200;
+let abstract_list = [];
+let abstract_nodes = document.getElementsByClassName('abstract');
+for (let i = 0; i < abstract_nodes.length; i++) {
+  let text = abstract_nodes[i].innerText;
   if (text.length > less_count) {
     /* Replace innerText with two spans*/
     abstract_nodes[i].innerText = ''
 
-    var less_text = document.createElement('span');
+    let less_text = document.createElement('span');
     less_text.innerText = text.slice(0, less_count) + '...';
     less_text.setAttribute('style', 'display: inline');
     abstract_nodes[i].appendChild(less_text);
 
-    var full_text = document.createElement('span');
+    let full_text = document.createElement('span');
     full_text.innerText = text;
     full_text.setAttribute('style', 'display: none');
     abstract_nodes[i].appendChild(full_text);
 
-    var a = document.createElement('a');
+    let a = document.createElement('a');
     a.setAttribute('href', 'javascript:toogleAbstract(' + abstract_list.length + ')');
     a.innerText = '[show more]';
     a.setAttribute('style', 'margin-left: 2px');
     abstract_nodes[i].appendChild(a);
 
-    abstract_list.push([less_text, full_text, a, false]); // bool indicated less (false) or full (true)
+    abstract_list.push([less_text, full_text, a, false]); // bool indicated less (false) or full (true).
   }
 }
 
 function toogleAbstract(idx) {
   item = abstract_list[idx];
-  if(item[3]) { // full text to less text
+  if (item[3]) { // full text to less text
     item[0].setAttribute('style', 'display: inline');
     item[1].setAttribute('style', 'display: none');
     item[2].innerText = '[show more]';
